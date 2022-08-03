ee48ec7cac11c5e4c900a13188d92b091e316ed7
Gik-op <73054589+Gik-op@users.noreply.github.com>
2022-08-03 21:26:19 +0800

    Update pe12-2a.h

--- chapter12/exercise2/pe12-2a.h
+++ chapter12/exercise2/pe12-2a.h
@@ -1,8 +1,6 @@
 #ifndef UNIT12EXER2_PE12_2A_H
 #define UNIT12EXER2_PE12_2A_H
 
-#endif //UNIT12EXER2_PE12_2A_H
-
 static int mode=0;
 static double distance;
 static double fuel;
@@ -10,3 +8,5 @@ static double fuel;
 void set_mode(int set);
 void get_info(void);
 void show_info(void);
+
+#endif //UNIT12EXER2_PE12_2A_H
