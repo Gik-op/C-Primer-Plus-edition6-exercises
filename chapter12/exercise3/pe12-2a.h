afb11199f11e145352c7ea007cd30b7dfe224f5c
Gik-op <73054589+Gik-op@users.noreply.github.com>
2022-08-03 21:26:57 +0800

    Update pe12-2a.h

--- chapter12/exercise3/pe12-2a.h
+++ chapter12/exercise3/pe12-2a.h
@@ -1,8 +1,8 @@
 #ifndef UNIT12EXER2_PE12_2A_H
 #define UNIT12EXER2_PE12_2A_H
 
-#endif //UNIT12EXER2_PE12_2A_H
-
 void set_mode(int set,int *ptr);
 void get_info(int mode,double *dis,double *fu);
 void show_info(int mode,double distance,double fuel);
+
+#endif //UNIT12EXER2_PE12_2A_H
